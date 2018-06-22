2c00420b4d64d6f7500618430bac73ddbfd78d4a
Rie Masukawa <rie.maskawa@gmail.com>
2018-06-22 17:06:22 +0900

    完成

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,27 @@ int n;
 int k;
 int A[100000];
 
+int max(int m){
+    int max=A[0];
+    for(int j=1; j≤n; j++){
+        if(A[0]<A[j]){
+            max=A[j];
+        }
+    }
+    return max;
+}
+
+int sum(int x){
+    int sum=0;
+    for(h=0; h<n; h++){
+        sum=sum+ceil(A[h]/x);
+    }
+    return sum;
+}
+
+int p(int x){
+    return(int)sum<=k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +32,19 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+    lb=0;
+    ub=max;
+    while(ub-lb>1){
+        int x=(lb+ub)/2;
+        if(p(x)){
+            ub=x;
+        }
+        else{
+            lb=x;
+        }
+    }
 
+    printf("%d\n",ub);
 
   return 0;
 }
